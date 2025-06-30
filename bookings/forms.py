18ca1b2064fe71e3f415dd7b0202ab9150e749ba
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-06-30 19:57:42 +0100

    feat: create BookingForm for booking data input

---
+++ bookings/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Booking
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['name', 'email', 'phone', 'date', 'time', 'party_size']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
