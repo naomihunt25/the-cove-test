a03a9788f9e636c92ddceb7008efda2443decaf4
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-06-30 21:01:15 +0100

    from django.views.decorators.http import require_POST

--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import BookingForm
 from .models import Booking
 from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def booking_create(request):
@@ -37,10 +38,17 @@ def booking_update(request, pk):
         form = BookingForm(instance=booking)
     return render(request, 'bookings/booking_form.html', {'form': form, 'update': True})
 
-from django.views.decorators.http import require_POST
-
 @require_POST
 def booking_delete(request, pk):
     booking = get_object_or_404(Booking, pk=pk)
     booking.delete()
     return redirect('booking_list')
+
+
+@login_required
+def booking_list(request):
+    if request.user.is_staff:
+        bookings = Booking.objects.all().order_by('-booking_date', '-booking_time')
+    else:
+        bookings = Booking.objects.filter(user=request.user).order_by('-booking_date', '-booking_time')
+    return render(request, 'bookings/booking_list.html', {'bookings': bookings})
