e11affb774d53da7e9148b1e057e3e0344ed440c
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-01 20:05:37 +0100

    chore: collect static files for Heroku deployment

--- thecove/settings.py
+++ thecove/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
