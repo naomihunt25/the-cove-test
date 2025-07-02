cd0ba00b4cb898d807c4b13cf73f4be2be3b8476
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-02 01:15:01 +0100

    log out

--- thecove/settings.py
+++ thecove/settings.py
@@ -130,3 +130,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'booking_list'
